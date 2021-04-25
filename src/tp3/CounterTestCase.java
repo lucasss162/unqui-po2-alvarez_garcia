ff4929d056c981152aad189b79546db56376ce10
Alfa <Alfa@Alfa>
2021-04-24 23:43:44 -0300

    Inicio en eclipse y tp3

---
+++ src/tp3/CounterTestCase.java
@@ -0,0 +1,14 @@
+package tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CounterTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
